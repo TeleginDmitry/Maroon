38f1b79f9ffd965a59ba3029dde4b6beed57d441
Deividas Vasilevskis <deivid@Deividass-MacBook-Pro.local>
2023-09-24 14:36:32 +0300

    Other files of project

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+    <React.StrictMode>
+    </React.StrictMode>
 );
- 
+
