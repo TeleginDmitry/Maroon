d2e08a497ff3562659ba787e37d7678379b160d0
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2023-09-24 16:18:05 +0300

    added nullStyle.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'assets/styles/mixins.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode></React.StrictMode>)
