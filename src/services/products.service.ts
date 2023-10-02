b3f2997ed5202be5135fd3bd69c174e7463c8b4f
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2023-10-02 20:20:42 +0300

    fixed bug with eslint

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,4 @@
 import { instance } from 'api/api.interceptor'
-// eslint-disable-next-line import/named
 import { AxiosResponse } from 'axios'
 import { Product } from 'shared/types/product.type'
 
