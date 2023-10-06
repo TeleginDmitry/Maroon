976b2aa75d8492f9373bd05af414f605ca2fad0f
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2023-10-06 15:49:47 +0300

    Исправил git actions

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,30 @@
 import { Outlet } from 'react-router-dom'
 import styles from './Layout.module.scss'
 import { Footer } from 'components/shared/footer/Footer'
+
+
+
 import { Header } from 'components/shared/header/Header'
 
+
+
+
 export function Layout() {
+
+
     return (
         <div className={styles.wrapper}>
             <Header></Header>
-            <main className={styles.content}>
+
+
+
+            <main className={
+                styles.content}>
                 <Outlet />
             </main>
+
+
+
             <Footer></Footer>
         </div>
     )
