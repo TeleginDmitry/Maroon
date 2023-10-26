11fdfdfd4822e314414bc08462217019329cd0e4
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2023-10-26 22:37:47 +0300

    Улучшил авторизацию и добавил страницу с пользователем

--- src/shared/validations/user.validate.ts
+++ src/shared/validations/user.validate.ts
@@ -1,8 +1,8 @@
-import { object, string, mixed } from 'yup';
+import { object, string, mixed } from 'yup'
 
 export const UserValidationSchema = object({
     email: string().email('Неправильный формат email').optional(),
     password: string().optional(),
     name: string().optional(),
-    image: mixed().optional(),
-});
+    image: mixed().optional()
+})
